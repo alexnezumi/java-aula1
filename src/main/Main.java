0ebd8bd3a5084b6b41f6357cb0453b7c557e97ea
alex <alexsilva.viana2006@gmail.com>
2024-09-16 18:37:39 -0300

    Initial Commit

---
+++ src/main/Main.java
@@ -0,0 +1,67 @@
+
+package main;
+
+import javax.swing.JOptionPane;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+   
+ 
+        
+        //atividade 1
+        
+       String nome = "ana";
+        double nota1 = 8.0, nota2 = 4.0, nota3 = 6.0; 
+        double resultado = (nota1 + nota2 + nota3)/3;
+        System.out.println("A aluna " + nome + " teve como média a nota " + resultado);
+              
+       //atividade 2
+       
+       int valor = 575;
+       int resultad = 575/10; 
+      System.out.println("10% de "+ valor + " é " + resultad);
+      
+      //atividade 3
+      
+      int x = 600, y = 500;
+      int z = x + y ;
+      System.out.println(z);
+      
+      int a = x * y ;
+      System.out.println(a);
+      
+      int b = x / y ;
+      System.out.println(b);
+      
+      int c = x - y ;
+      System.out.println(c);
+      
+      //atividade 4
+      
+     /*
+      int a = 5 * 1 ;
+      int b = 5 * 2; 
+      int c = 5 * 3; 
+      int d = 5 * 4;
+      int e = 5 * 5; 
+      int f = 5 * 6; 
+      int g = 5 * 7; 
+      int h = 5 * 8;
+      int i = 5 * 9;
+      int j = 5 * 10;
+      
+      System.out.println("5 * 1 = " + a );
+      System.out.println("5 * 2 = " + b );
+      System.out.println("5 * 3 = " + c );
+      System.out.println("5 * 4 = " + d );
+      System.out.println("5 * 5 = " + e );
+      System.out.println("5 * 6 = " + f );
+      System.out.println("5 * 7 = " + g );
+      System.out.println("5 * 8 = " + h );
+      System.out.println("5 * 9 = " + i );
+      System.out.println("5 * 10 = " + j );
+*/
+    }
+}
